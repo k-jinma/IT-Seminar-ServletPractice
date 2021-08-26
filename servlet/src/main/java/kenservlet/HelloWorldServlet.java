249ea4eee5d4afb4edb174dd08fa94a79fe7c1e0
k-jinma <jinmakazuhiro@outlook.jp>
2021-08-26 23:46:09 +0900

    initial commit

---
+++ servlet/src/main/java/kenservlet/HelloWorldServlet.java
@@ -0,0 +1,36 @@
+package kenservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// レスポンスのコンテンツタイプを指定する
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>タイトル</h1>");
+		out.println("<p>こんにちは！</p>");
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+		
+	}
+
+}
