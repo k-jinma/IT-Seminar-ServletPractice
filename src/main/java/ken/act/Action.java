d9a78225d85c52e310ef793976e1d34214f1709e
kazuuud <81429095+k-jinma@users.noreply.github.com>
2021-09-30 17:27:15 +0900

    Delete src/main directory

--- src/main/java/ken/act/Action.java
+++
@@ -1,8 +0,0 @@
-package ken.act;
-
-import javax.servlet.http.HttpServletRequest;
-
-public abstract class Action {
-	//抽象メソッド
-	abstract public String execute(HttpServletRequest request) throws Exception;
-}
