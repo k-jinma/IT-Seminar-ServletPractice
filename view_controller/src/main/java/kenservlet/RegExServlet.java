e7f0ee10f97d131884018fed3cb9ccf1a928b88c
kazuuud <81429095+k-jinma@users.noreply.github.com>
2021-12-15 13:45:42 +0900

    自作メソッドを作成した改良版

--- view_controller/src/main/java/kenservlet/RegExServlet.java
+++ view_controller/src/main/java/kenservlet/RegExServlet.java
@@ -23,42 +23,69 @@ public class RegExServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// リクエスト処理
+
 		request.setAttribute("message", "入力してください");
-		
+
 		ServletContext sc = getServletContext();
-		RequestDispatcher rd = sc.getRequestDispatcher("/WEB-INF/regexInput.jsp");
+		RequestDispatcher rd = sc.getRequestDispatcher("/regexInput.jsp");
 		rd.forward(request, response);
-		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// レスポンス処理
-		request.setCharacterEncoding("UTF-8");
+
+
+		request.setCharacterEncoding("utf-8");
 		String param = request.getParameter("zip");
-		String regex = "^\\d{3}-\\d{4}$";
-		
+		String param2 = request.getParameter("tel");
+
+		// ^ここからはじまり $ここで終わる　という意味
+		String regex = "^\\d{3}-\\d{4}$";	// ^[0-9]{3}-[0-9]{4}$
+		String regex2 = "^[0-9]{3}-[0-9]{4}-[0-9]{4}$";
+
 		String message = "";
-		if(param != null && !param.isEmpty()) {
-			Pattern pattern = Pattern.compile(regex);
-			Matcher matcher = pattern.matcher(param);
-			if( matcher.matches()) {
+
+		//不正アクセスでなく、空欄でなければ
+		if( param != null && !param.isEmpty() ) {
+
+			//検証する
+			if( match(regex , param) && match(regex2, param2) ) {
 				message = "登録完了しました";
+
 			}else {
 				message = "不適切な入力です";
 			}
 		}else {
 			message = "入力は必須です";
 		}
-		
+
 		request.setAttribute("message", message);
-		
+
 		ServletContext sc = getServletContext();
-		RequestDispatcher rd = sc.getRequestDispatcher("/WEB-INF/regexInput.jsp");
+		RequestDispatcher rd = sc.getRequestDispatcher("/regexInput.jsp");
 		rd.forward(request, response);
+	}
+
+
+	/**
+	 * パターンマッチを判定するメソッドを自作
+	 * 引数: regex 正規表現文字列
+	 * 引数: param 検証文字列
+	 * 戻り値： パターンに一致していればtrue、不一致はfalseを返す
+	 */
+	public boolean match( String regex, String param ) {
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(param);
+
+		if( matcher.matches() ) {
+			return true;
+
+		}else {
+			return false;
+		}
+
 	}
 
 }
